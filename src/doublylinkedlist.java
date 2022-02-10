56f312435667e31eeb5ab62610a415ac224a99d3
HP <shivangi.agrawal_cs.aiml20@gla.ac.in>
2022-02-10 22:18:51 +0530

    Initial commit

--- src/doublylinkedlist.java
+++ src/doublylinkedlist.java
@@ -12,6 +12,20 @@ class Node1{
 }
 public class doublylinkedlist {
     Node1 head;
+    Node1 tail;
+    public void insertatBeg(Node1 newnode){
+        if(head==null){
+            head=tail=newnode;
+            head.prev=null;
+            tail.next=null;
+        }
+        else{
+            head.prev= newnode;
+            newnode.next=head;
+            newnode.prev=null;
+            head= newnode;
+        }
+    }
     public void insertatLast(Node1 newnode){
         if(head==null){
             head= newnode;
@@ -23,6 +37,21 @@ public class doublylinkedlist {
             }
             temp.next=newnode;
             newnode.prev=temp;
+        }
+    }
+    public void insertPos(int pos,Node1 newnode){
+        if(head==null){
+            head=newnode;
+        }
+        else {
+            Node1 temp= head;
+            for (int i=1;i<pos-1;i++){
+                temp=temp.next;
+            }
+            newnode.next=temp.next;
+            temp.next=newnode;
+            newnode.prev= temp;
+
         }
     }
     public void deletefromBeg(){
@@ -55,23 +84,23 @@ class Main1{
             System.out.println("""
                     Enter your choice\s
                      1) To insert at Beginning\s
-                     2) To insert at last\s
-                     3) To print the LinkedList\s
-                     4) To insert at specific position\s
+                     2) to insert at last\s
+                     3) to insert at specific position\s
+                     4) To print the LinkedList\s
                      5) to delete from beginning\s
                      6) to delete from last\s
-                     
-                     7) exit""");
+                     7) to 
+                     8) exit""");
             System.out.println("Enter your choice");
             int c = sc.nextInt();
             switch (c) {
-               // case 1 -> dll.i
+                case 1 -> dll.insertatBeg(new Node1(sc.nextInt()));
                 case 2 -> dll.insertatLast(new Node1(sc.nextInt()));
-                case 3 -> dll.display();
-               // case 4 -> sll.insertatPos(sc.nextInt(), new Node(sc.nextInt()));
+                case 3 -> dll.insertPos(sc.nextInt(), new Node1(sc.nextInt()));
+                case 4 -> dll.display();
                 case 5 -> dll.deletefromBeg();
                // case 6 -> sll.deleteatLast();
-                case 7 -> System.exit(1);
+                case 8 -> System.exit(1);
             }
         }
     }
