c2a7a67949673af21b4e7b4a6199086382de4797
HP <shivangi.agrawal_cs.aiml20@gla.ac.in>
2022-02-10 20:37:47 +0530

    Initial commit

--- src/singlylinkedlist.java
+++ src/singlylinkedlist.java
@@ -8,193 +8,178 @@ class Node{
         this.next=null;
     }
 }
-class singlylinkedlist{
+class singlylinkedlist {
     Node head;
-    public void insertatBeg(Node newnode){
+
+    public void insertatBeg(Node newnode) {
         if (head != null) {
             newnode.next = head;
         }
-        head= newnode;
+        head = newnode;
         System.out.println("Node inserted");
     }
-    public void insertatEnd(Node newnode){
-        if(head==null){
-            head=newnode;
-        }
-        else{
-            Node temp=head;
-            while(temp.next!=null){
-                temp=temp.next;
+
+    public void insertatEnd(Node newnode) {
+        if (head == null) {
+            head = newnode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
             }
-            temp.next=newnode;
+            temp.next = newnode;
         }
         System.out.println("Node inserted");
     }
-    public void insertatPos(int pos,Node newnode){
-        if(head==null){
-            head=newnode;
-        }
-        else{
-            Node temp= head;
-            for (int i=1;i<pos-1;i++){
-                temp=temp.next;
+
+    public void insertatPos(int pos, Node newnode) {
+        if (head == null) {
+            head = newnode;
+        } else {
+            Node temp = head;
+            for (int i = 1; i < pos - 1; i++) {
+                temp = temp.next;
             }
-            newnode.next=temp.next;
-            temp.next= newnode;
+            newnode.next = temp.next;
+            temp.next = newnode;
         }
         System.out.println("Node inserted");
     }
-    public void traverse(){
-        if(head==null){
+
+    public void traverse() {
+        if (head == null) {
             System.out.println("No element Available");
-        }
-        else{
-            Node temp= head;
-            while(temp!=null){
+        } else {
+            Node temp = head;
+            while (temp != null) {
                 System.out.println(temp.data);
-                temp=temp.next;
+                temp = temp.next;
             }
         }
     }
-    public void deletefrombeg(){
+
+    public void deletefrombeg() {
         if (head == null) {
             System.out.println("No element available");
-        }
-        else{
-            head=head.next;
+        } else {
+            head = head.next;
         }
         System.out.println("Node deleted");
     }
-    public void deleteLast(){
-        if(head!=null){
-            Node temp=head;
-            Node prev=null;
-            while (temp!=null){
-                prev=temp;
-                temp=temp.next;
+
+    public void deleteLast() {
+        if (head != null) {
+            Node temp = head;
+            Node prev = null;
+            while (temp != null) {
+                prev = temp;
+                temp = temp.next;
             }
-            prev.next=null;
+            prev.next = null;
         }
         System.out.println("Node deleted");
     }
-    public void deleteatPos(int pos){
-        if(head==null){
+
+    public void deleteatPos(int pos) {
+        if (head == null) {
             System.out.println("No element to delete");
         }
-        if(pos==0){
-            head=head.next;
-        }
-        else {
-            Node temp=head;
-            for (int i=1;i<pos-1 && temp!=null;i++){
-                temp=temp.next;
+        if (pos == 0) {
+            head = head.next;
+        } else {
+            Node temp = head;
+            for (int i = 1; i < pos - 1 && temp != null; i++) {
+                temp = temp.next;
             }
-            if(temp==null || temp.next==null){
+            if (temp == null || temp.next == null) {
                 System.out.println("No element to delete");
+            } else {
+                temp.next = temp.next.next;
             }
-            else {
-            temp.next= temp.next.next;}
         }
         System.out.println("Node deleted");
     }
-    public void search(int ele){
-        Node temp= head;
-        int c=0;
-        while (temp!= null){
-            if(temp.data==ele){
+
+    public void search(int ele) {
+        Node temp = head;
+        int c = 0;
+        while (temp != null) {
+            if (temp.data == ele) {
                 System.out.println("Element found");
-                c=1;
+                c = 1;
                 break;
             }
-            temp=temp.next;
+            temp = temp.next;
         }
-        if (c==0){
+        if (c == 0) {
             System.out.println("Element not found");
         }
     }
-    public void sortasc(){
-        Node one= head;
-        while (one!=null){
-            Node two= one.next;
-            while (two!=null){
-                if(one.data>two.data){
-                    int t= one.data;
-                    one.data= two.data;
-                    two.data=t;
+
+    public void sortasc() {
+        Node one = head;
+        while (one != null) {
+            Node two = one.next;
+            while (two != null) {
+                if (one.data > two.data) {
+                    int t = one.data;
+                    one.data = two.data;
+                    two.data = t;
                 }
-                two=two.next;
+                two = two.next;
             }
-            one=one.next;
+            one = one.next;
         }
         System.out.println("List sorted");
     }
-    public void sorted(){
-        Node one= head;
-        while (one!=null){
-            Node two= one.next;
-            while (two!=null){
-                if(one.data<two.data){
-                    int t= one.data;
-                    one.data= two.data;
-                    two.data=t;
+
+    public void sorted() {
+        Node one = head;
+        while (one != null) {
+            Node two = one.next;
+            while (two != null) {
+                if (one.data < two.data) {
+                    int t = one.data;
+                    one.data = two.data;
+                    two.data = t;
                 }
-                two=two.next;
+                two = two.next;
             }
-            one=one.next;
+            one = one.next;
         }
         System.out.println("List sorted");
     }
 
     public void mergelists(Node l1,Node l2) {
-        Node result = null;
-        while(l1 != null || l2 != null) {
-            int minVal;
-            if (l1 == null) {
-                minVal = l2.data;
-                l2 = l2.next;
-            } else if (l2 == null) {
-                minVal = l1.data;
-                l1 = l1.next;
-            } else if(l1.data <= l2.data) {
-                minVal = l1.data;
-                l1 = l1.next;
-            } else {
-                minVal = l2.data;
-                l2 = l2.next;
-            }
-
-            if(result == null) {
-                result = head = new Node(minVal);
-            } else {
-                result.next = new Node(minVal);
-                result = result.next;
-            }
+        while (l1.next!=null){
+            l1= l1.next;
         }
-        System.out.println("List Merged. Check by typing 3!!");
+        l1.next=l2;
     }
 }
+
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         singlylinkedlist sll = new singlylinkedlist();
-        singlylinkedlist sll2= new singlylinkedlist();
-        sll2.insertatEnd(new Node(1));
-        sll2.insertatEnd(new Node(2));
-        sll2.insertatEnd(new Node(3));
-        sll2.insertatEnd(new Node(4));
-        sll2.insertatEnd(new Node(5));
-        sll2.insertatEnd(new Node(6));
-        Node n1 = new Node(10);
+        sll.head = new Node(10);
         Node n2 = new Node(20);            // this is optional I have given the list before, or we can build it during runtime by scanner
         Node n3 = new Node(30);            // initialized linked list
         Node n4 = new Node(40);
         Node n5 = new Node(50);
-        n1.next = n2;
+        sll.head.next = n2;
         n2.next = n3;
         n3.next = n4;
         n4.next = n5;
         n5.next = null;
-        sll.head = n1;
+        singlylinkedlist sll2 = new singlylinkedlist();
+        sll2.head= new Node(5);
+        sll2.insertatEnd(new Node(44));
+        sll2.insertatEnd(new Node(3));
+        sll2.insertatEnd(new Node(2));
+        sll2.insertatEnd(new Node(1));
+        sll2.insertatEnd(new Node(0));
+
         while (true) {
             System.out.println("""
                     Enter your choice\s
@@ -224,7 +209,7 @@ class Main {
                 case 8 -> sll.deleteatPos(sc.nextInt());
                 case 9 -> sll.sortasc();
                 case 10 -> sll.sorted();
-                case 11 -> sll.mergelists(sll2.head,sll.head);
+                case 11 -> sll.mergelists(sll.head, sll2.head);
                 case 12 -> sll.search(sc.nextInt());
                 case 13 -> System.exit(1);
             }
