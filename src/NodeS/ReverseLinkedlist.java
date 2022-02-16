80419150c47c0af7ac21d29d29ba2739d58d45c5
HP <shivangi.agrawal_cs.aiml20@gla.ac.in>
2022-02-16 10:10:17 +0530

    Initial commit

--- src/NodeS/ReverseLinkedlist.java
+++ src/NodeS/ReverseLinkedlist.java
@@ -3,11 +3,56 @@ package NodeS;
 import java.util.Scanner;
 
 public class ReverseLinkedlist {
+    nodes head;
+    nodes tail;
+    int c=0;
+    int [] arr= new int[1000];
+    public void insert(nodes newfound){
+        if (newfound.data != -1) {
+            if (head == null) {
+                head =tail= newfound;
+            } else {
+                tail.next = newfound;
+                tail=newfound;
+            }
 
+            c++;
+        }
+        else {
+            tail = newfound;
+            tail.data = -1;
+
+        }
+    }
+    public void reverse(){
+        int k=0;
+        nodes temp= head;
+        while (temp!=null){
+            arr[c-k]= temp.data;
+            temp=temp.next;
+            k++;
+        }
+        for (int j=1;j<k+1;j++) {
+            System.out.print(arr[j]+" ");
+        }
+    }
 }
 class M{
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
-
+        Scanner sc= new Scanner(System.in);
+        ReverseLinkedlist rll= new ReverseLinkedlist();
+        System.out.println("Input the no. of testcases");
+        int t = sc.nextInt();
+        int i = 0;
+        while (i < t) {
+            while (rll.tail == null || rll.tail.data != -1) {
+                rll.insert(new nodes(sc.nextInt()));
+            }
+            rll.reverse();
+            rll.head = null;
+            rll.tail = null;
+            rll.c = 0;
+            i++;
+        }
     }
 }
